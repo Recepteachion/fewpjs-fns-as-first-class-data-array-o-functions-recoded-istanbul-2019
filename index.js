04836f6f6f105985709643ccde2d584529354da4
Recep Öztürk <turkishteacher23@gmail.com>
2020-01-19 19:07:42 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,37 @@
+function wakeDog(dogName,dogBreed){
+	return(`Wake ${dogName} the ${dogBreed}`);
+}
+
+function leashDog(dogName,dogBreed){
+	return(`Leash ${dogName} the ${dogBreed}`);
+}
+
+function walkToPark(dogName,dogBreed){
+	return(`Walk ${dogName} the ${dogBreed}`);
+}
+
+function throwFrisbee(dogName,dogBreed){
+	return(`Throw frisbee to ${dogName} the ${dogBreed}`);
+}
+
+function walkHome(dogName,dogBreed){
+	return(`Walk ${dogName} the ${dogBreed} to home`);
+}
+
+function unleashDog(dogName,dogBreed){
+	return (`Unleash ${dogName} the ${dogBreed}`);
+
+}
+
+
+let routine = [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog]
+
+function exerciseDog(dogName,dogBreed){
+	let arr = [];
+	for (let i=0; i <routine.length; i++){
+		
+		let dg = routine[i](dogName,dogBreed);
+			
+		arr.push(dg)	
+	} return arr
+}
